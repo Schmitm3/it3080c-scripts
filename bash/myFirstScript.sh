54cec60a29195751f225bfb0c7eb8f64b96b46ec
Matt Schmitt <schmitm3@mail.uc.edu>
2021-01-19 19:28:14 -0500

    My first commit

---
+++ bash/myFirstScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script outputs the IP address and hostname of a machine
+
+echo 'This is a script. Hello!'
+echo Machine Type: $MACHTYPE
+echo Hostname: $HOSTNAME
+echo Working Dir: $PWD
+echo Session Length: $SECONDS
+echo Home Dir: $HOME
+
+a=$(ip a | grep 'dynamic ens192' | awk '{print $2}')
+echo My IP address is $a
