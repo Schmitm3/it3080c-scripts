ed4b2942793a66d11455571478d6af5b316e07f9
Matt Schmitt <schmitm3@gmail.com>
2021-02-19 12:37:44 -0500

    project 1 final

---
+++ project1/GetUserDomain.ps1
@@ -0,0 +1,27 @@
+param(
+    [Parameter(Mandatory=$true)][string]$UserFile
+)
+
+###Creating a transcript of the PowerShell
+Start-Transcript -Path ".\transcript.log"
+
+###Getting CSV File to and User List to run
+###This reolves the path of the file so we do not need to hard code
+$userFilePath = resolve-path $UserFile
+$userList = Import-CSV $userFilePath
+$domainList = ".\DomainList.csv"
+$labels = ("Email", "Domain") -join ","
+Add-Content -Path $domainList -Value $labels -Encoding UTF8
+
+###Creating For Loop to cycle through each entry in teh CSV file
+foreach ($user in $userList)
+{
+    $email = $user.Email
+
+    $domain = $email.Split("@")[1]
+    $results = ("$email", "$domain") -join ","
+    Add-Content -Path $domainList -Value $results -Encoding UTF8
+}
+
+###Stopping Transcript
+Stop-Transcript
